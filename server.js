39d313694507f867f0522203a3928d61921cf0a2
Donkey <danglayers@gmail.com>
2020-09-18 15:32:40 -0400

    fixed broken link

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
+  
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const Workout = require("./models");
-const app = express();
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useUnifiedTopology: true, 
+  useCreateIndex: true, 
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes for the html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}. . .`);
+});
