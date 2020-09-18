0dfeda97be5f89e50a53418f74ed9fef8a31986c
Donkey <danglayers@gmail.com>
2020-09-18 15:31:32 -0400

    made some changes to models

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// this will create an exercise schema to be used as the child of the workout chema
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true, 
+        },
+    }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    let total = 0;
+    console.log(total);
+    for (let i = 0; i < this.exercises.length; i++) {
+        total += this.exercises[i].duration;
+    }
+    return total;
+});
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
